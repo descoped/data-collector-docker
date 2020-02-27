f22ebb70d952bbfa1b9b224b9e664db92a6db03b
oranheim <oranheim@gmail.com>
2020-02-27 16:17:30 +0100

    Skip testing when building docker image

--- build-dev.sh
+++ build-dev.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mvn clean verify dependency:copy-dependencies &&\
+mvn clean verify dependency:copy-dependencies -DskipTests &&\
 docker build -t data-collector:dev -f Dockerfile-dev .
