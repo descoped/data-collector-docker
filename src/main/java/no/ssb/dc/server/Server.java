8e62c6f43774ea5b13f2020df09d7152b91a83ee
oranheim <oranheim@gmail.com>
2020-11-06 10:59:41 +0100

    Boodstrap Google Secret suppliers during server startup

--- src/main/java/no/ssb/dc/server/Server.java
+++ src/main/java/no/ssb/dc/server/Server.java
@@ -3,11 +3,19 @@ package no.ssb.dc.server;
 import ch.qos.logback.classic.ClassicConstants;
 import net.bytebuddy.agent.ByteBuddyAgent;
 import no.ssb.config.StoreBasedDynamicConfiguration;
+import no.ssb.dapla.secrets.api.SecretManagerClient;
+import no.ssb.dc.api.config.BusinessSSLBundle;
+import no.ssb.dc.api.config.ContentStreamEncryptionCredentials;
 import no.ssb.dc.application.server.UndertowApplication;
 import no.ssb.dc.core.metrics.MetricsAgent;
 import no.ssb.dc.core.util.JavaUtilLoggerBridge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Supplier;
 
 public class Server {
 
@@ -25,7 +33,7 @@ public class Server {
         }
         JavaUtilLoggerBridge.installJavaUtilLoggerBridgeHandler();
         if (logbackConfigurationFile != null) {
-            LOG.debug("Using logback configuration: {}" , logbackConfigurationFile);
+            LOG.debug("Using logback configuration: {}", logbackConfigurationFile);
         }
 
         StoreBasedDynamicConfiguration.Builder configurationBuilder = new StoreBasedDynamicConfiguration.Builder()
@@ -33,7 +41,6 @@ public class Server {
                 .propertiesResource("/conf/application-defaults.properties")
                 .propertiesResource("/conf/application.properties")
                 .propertiesResource("/conf/application-override.properties");
-
         /*
          * pre-req: `docker-compose up` in this repo
          */
@@ -54,7 +61,74 @@ public class Server {
                 .systemProperties();
 
         StoreBasedDynamicConfiguration configuration = configurationBuilder.build();
-        UndertowApplication application = UndertowApplication.initializeUndertowApplication(configuration);
+
+
+        Supplier<ContentStreamEncryptionCredentials> contentEncryptionCredentialsSupplier = () -> {
+            if (true/*check IF NOT use delegated encryption credentials config*/) {
+                return null;
+            }
+            // TODO config mapping
+            Map<String, String> providerConfiguration = Map.of(
+                    "secrets.provider", "google-secret-manager",
+                    "secrets.projectId", "ssb-team-dapla",
+                    "secrets.serviceAccountKeyPath", "FULL_PATH_TO_SERVICE_ACCOUNT.json"
+            );
+            try (SecretManagerClient secretManagerClient = SecretManagerClient.create(providerConfiguration)) {
+                return new ContentStreamEncryptionCredentials() {
+                    @Override
+                    public char[] key() {
+                        final byte[] encryptionKey = secretManagerClient.readBytes("secrets.encryptionKey");
+                        try {
+                            return ByteBuffer.wrap(encryptionKey).asCharBuffer().array();
+                        } finally {
+                            Arrays.fill(encryptionKey, (byte) 0);
+                        }
+                    }
+
+                    @Override
+                    public byte[] salt() {
+                        final byte[] encryptionSalt = secretManagerClient.readBytes("secrets.encryptionSalt");
+                        try {
+                            return encryptionSalt;
+                        } finally {
+                            Arrays.fill(encryptionSalt, (byte) 0);
+                        }
+                    }
+                };
+            }
+        };
+
+        Supplier<BusinessSSLBundle> sslBundleSupplier = () -> {
+            if (true/*check IF NOT use delegated sslBundle config*/) {
+                return null;
+            }
+            // TODO config mapping
+            Map<String, String> providerConfiguration = Map.of(
+                    "secrets.provider", "google-secret-manager",
+                    "secrets.projectId", "ssb-team-dapla",
+                    "secrets.serviceAccountKeyPath", "FULL_PATH_TO_SERVICE_ACCOUNT.json"
+            );
+            try (SecretManagerClient secretManagerClient = SecretManagerClient.create(providerConfiguration)) {
+                return new BusinessSSLBundle() {
+                    @Override
+                    public byte[] publicCertificate() {
+                        return new byte[0];
+                    }
+
+                    @Override
+                    public byte[] privateCertificate() {
+                        return new byte[0];
+                    }
+
+                    @Override
+                    public byte[] passphrase() {
+                        return new byte[0];
+                    }
+                };
+            }
+        };
+
+        UndertowApplication application = UndertowApplication.initializeUndertowApplication(configuration, contentEncryptionCredentialsSupplier, sslBundleSupplier);
 
         try {
             Runtime.getRuntime().addShutdownHook(new Thread(() -> {
